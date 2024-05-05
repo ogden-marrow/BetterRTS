39fc3b19ab6661d51ee7b6c675bff4fc7b0798ff
John Palenchar <jpalenchar.3@gmail.com>
2024-05-05 18:13:02 -0400

    Update to fix ServiceBulletin

--- API/main.py
+++ API/main.py
@@ -5,16 +5,17 @@ from secrets import API_KEY as api_key
 def main():
     def print_routes(routes):
         for route in routes:
-            print("route")
+            print(route)
             on_route = STP.get_vehicles_with_locations(api_key, route.rt, completion=print_vehicles)
-            print("on_route")
+            print(on_route)
 
     def print_vehicles(vehicles):
-        print("vehicles")
+        print(vehicles)
 
     def print_service_bulletins(bulletins):
         print(bulletins)
 
+    STP.get_routes(api_key,completion=print_routes)
     STP.get_service_bulletins(api_key, "5", completion=print_service_bulletins)
 
 
